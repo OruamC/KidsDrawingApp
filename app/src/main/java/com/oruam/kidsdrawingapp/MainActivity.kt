35c8d6bd083b244ef2ffaca186799201181f6635
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-09-21 18:16:59 -0300

    Setting the colors selections

--- app/src/main/java/com/oruam/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/oruam/kidsdrawingapp/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.oruam.kidsdrawingapp
 
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
-    private var drawingView : DrawingView? = null
+    private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +20,14 @@ class MainActivity : AppCompatActivity() {
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
 
-        val ibBrush : ImageButton = findViewById(R.id.ib_brush)
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
+        mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
+
+        val ibBrush: ImageButton = findViewById(R.id.ib_brush)
         ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
